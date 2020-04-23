4689962e7174486493ae8aa4427dab9d6bbc253a
Mati <mateuszlibero@gmail.com>
2020-04-23 21:56:25 +0200

    switched from fastify to express with ts support

---
+++ backend/src/app.ts
@@ -0,0 +1,36 @@
+import * as express from 'express'
+import { Application } from 'express'
+
+class App {
+    public app: Application
+    public port: number
+
+    constructor(appInit: { port: number; middleWares: any; controllers: any; }) {
+        this.app = express()
+        this.port = appInit.port
+
+        this.routes(appInit.controllers);
+        this.middlewares(appInit.middleWares)
+    }
+
+
+    private routes(controllers: { forEach: (arg0: (controller: any) => void) => void; }) {
+        controllers.forEach(controller => {
+            this.app.use('/', controller.router)
+        })
+    }
+
+    private middlewares(middleWares: { forEach: (arg0: (middleWare: any) => void) => void; }) {
+        middleWares.forEach(middleWare => {
+            this.app.use(middleWare)
+        })
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the http://localhost:${this.port}`)
+        })
+    }
+}
+
+export default App
